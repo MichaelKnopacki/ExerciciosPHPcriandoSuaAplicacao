4228d62e1b8d834895ead033f260402a1225fd71
Michael Knopacki <michaelknopacki@hotmail.com>
2025-11-20 07:53:58 -0300

    20251125(1)

---
+++ ExerciciosPHPcriandoSuaAplicacao.php
@@ -0,0 +1,19 @@
+-- 02. Catalogo de filmes
+
+<?php
+/* Defina variáveis com informações sobre o filme, como nome, ano de lançamento, nota e se ele está incluído no plano.
+Realize operações como soma e divisão para, com várias notas, chegar a uma média.
+Utilize operadores lógicos para definir se o filme está incluído no plano.
+Utilize de concatenação e interpolação de strings para formatar a exibição dos dados do filme.*/
+
+
+
+// ----------------------------------------------------------------------------------------------------
+
+
+
+
+?>
+
+
+
